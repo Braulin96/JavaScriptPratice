ea41225a97856dbae57ee2587d05abce046b1850
Braulin Pires <braulinpires@hotmail.com>
2023-09-12 22:39:36 +0100

    added events with onchange

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [selectedProduct, setSelectedProduct] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleChange = (e) => {
+    setSelectedProduct(e.target.value);
+  }
+
+  const handleChangeOption = (e) => {
+    setSelectedOption(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Select the product you want</h1>
+      <div className="flex" style={{textAlign:'center'}}>
+        <select id="framework" className="" onChange={handleChange}>
+          <option value="angular">Angular</option>
+          <option value="react">React</option>
+          <option value="Vue">Vue.js</option>
+          <option value="Ember">Ember.js</option>
+        </select>
+      </div>
+      <div className="flex" style={{textAlign:'center'}}>
+        <select id="framework" className="" onChange={handleChangeOption}>
+          <option value="1">option 1</option>
+          <option value="2">Option 2</option>
+          <option value="3">option 3</option>
+          <option value="4">option 4</option>
+        </select>
+      </div>
+        <p>You selected: {selectedProduct}</p>
+        <p>You selected: {selectedOption}</p>
     </div>
   );
 }
