a06e0fbd93df3b80438ac61f479a840346918315
Braulin Pires <braulinpires@hotmail.com>
2023-09-12 23:56:17 +0100

    created a function to chose product and option to sell and buy

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
+  const alertCompra = () => {
+    window.alert("Compra realizada com sucesso");
+  };
+  const alertVenda = () => {
+    window.alert("Venda realizada com sucesso");
+  };
 
   const [selectedProduct, setSelectedProduct] = useState("");
-  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedAction, setSelectedAction] = useState("");
 
-  const handleChange = (e) => {
-    setSelectedProduct(e.target.value);
-  }
+  const handleProductChange = (event) => {
+    setSelectedProduct(event.target.value);
+    setSelectedAction(""); // Reset the selected action when the product changes
+  };
 
-  const handleChangeOption = (e) => {
-    setSelectedOption(e.target.value);
-  }
+  const handleActionChange = (event) => {
+    setSelectedAction(event.target.value);
+  };
 
   return (
-    <div className="App">
-      <h1> Select the product you want</h1>
-      <div className="flex" style={{textAlign:'center'}}>
-        <select id="framework" className="" onChange={handleChange}>
-          <option value="angular">Angular</option>
-          <option value="react">React</option>
-          <option value="Vue">Vue.js</option>
-          <option value="Ember">Ember.js</option>
+    <>
+      <div className="App">
+        <h1>Selecione o produto desejado</h1>
+        <select id="productSelect" onChange={handleProductChange}>
+          <option value="">Selecione um Produto</option>
+          <option value="batata">Batata</option>
+          <option value="mandioca">Mandioca</option>
+          <option value="repolho">Repolho</option>
         </select>
+        {selectedProduct && (
+          <>
+            <p>O produto selecionado foi {selectedProduct}</p>
+            <label>Selecione a ação desejada</label>
+            <select id="actionSelect" onChange={handleActionChange}>
+              <option value="">Selecione uma Ação</option>
+              <option value="compra">Compra</option>
+              <option value="venda">Venda</option>
+            </select>
+          </>
+        )}
+        {selectedAction && (
+          <>
+            <label>Selecione a opção desejada</label>
+            {selectedAction === "compra" && (
+              <button onClick={alertCompra}>Comprar</button>
+            )}
+            {selectedAction === "venda" && (
+              <button onClick={alertVenda}>Vender</button>
+            )}
+          </>
+        )}
       </div>
-      <div className="flex" style={{textAlign:'center'}}>
-        <select id="framework" className="" onChange={handleChangeOption}>
-          <option value="1">option 1</option>
-          <option value="2">Option 2</option>
-          <option value="3">option 3</option>
-          <option value="4">option 4</option>
-        </select>
-      </div>
-        <p>You selected: {selectedProduct}</p>
-        <p>You selected: {selectedOption}</p>
-    </div>
+    </>
   );
 }
 
