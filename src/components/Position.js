dffef29c1fa565ee99ccbe6406d782ac982cac49
Braulin Pires <braulinpires@hotmail.com>
2023-09-21 20:53:56 +0100

    get the desciption depending on the title clicked

--- src/components/Position.js
+++ src/components/Position.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BlackListener from "../assets/blackListener.png";
 import Escutador from "../assets/escutador.png";
 import Mobile from "../assets/mobile.png";
@@ -6,34 +6,67 @@ import Mobile from "../assets/mobile.png";
 const arrayTest = [
   {
     title: "Image 1",
+    description: "description for Image 1",
     Image: BlackListener,
   },
   {
     title: "Image 2",
+    description: "description for Image 2",
     Image: Escutador,
   },
   {
     title: "Image 3",
+    description: "description for Image 3",
     Image: Mobile,
   },
 ];
 
 const Position = () => {
+  const [infoIndex, setInfoIndex] = useState(0); 
+
+  const handleChange = (index) => {
+    setInfoIndex(index); // Set infoIndex to the clicked index
+    console.log("infoIndex:", index);
+  };
+  
+
   return (
     <>
       <div className="flex p-20 text-center">
         <div className="bg-green-200 flex items-center justify-center rounded-2xl w-[300px] aspect-square">
-          <ul>
+          <ul className="space-y-5">
             {arrayTest.map((item, index) => (
-              <li key={index}>{item.title}</li>
+              <li
+                key={index}
+                onClick={() => handleChange(index)} // Pass the index to handleChange
+                className={`focus:text-red-400 ${
+                    infoIndex === index ? "text-red-400" : ""
+                  }`}
+              >
+                {item.title}
+              </li>
             ))}
           </ul>
         </div>
-        <div className="bg-green-400 w-[600px] rounded-2xl">image</div>
+        <div className="bg-green-400 w-[600px] rounded-2xl">
+            {arrayTest[infoIndex].description}
+        </div>
       </div>
-      <div className="p-20 flex">
+    </>
+  );
+};
+export default Position;
+
+
+{
+  /* 
+<div className="p-20 flex">
         <div className="container relative p-5 bg-white w-[300px] aspect-square rounded-2xl space-y-5 overflow-scroll">
-          <p>Position</p>
+          <div className="circle bg-blue-500 w-[400px] aspect-square bottom-2 blur-2xl absolute py-10 text-center rounded-full opacity-20 "></div>
+        </div>
+      </div>
+ <p>Position</p>
+
           <p>
             As plataformas digitais da RTP utilizam cookies próprios e de
             terceiros (Fornecedores) para melhorar o respetivo desempenho e a
@@ -42,10 +75,5 @@ const Position = () => {
             dispositivo, e também processando dados pessoais (em detalhe
             consultar "Política de Cookies" da RTP).
           </p>
-          <div className="circle bg-blue-500 w-[400px] aspect-square bottom-2 blur-2xl absolute py-10 text-center rounded-full opacity-20 "></div>
-        </div>
-      </div>
-    </>
-  );
-};
-export default Position;
+*/
+}
