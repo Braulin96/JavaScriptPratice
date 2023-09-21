1241e4ae27aab36db6a96ec63b8ad71cf6139360
Braulin Pires <braulinpires@hotmail.com>
2023-09-21 11:30:09 +0100

    added Buy component outside App component and styled the component with tailwind

--- src/App.js
+++ src/App.js
@@ -1,61 +1,12 @@
-import React, { useState } from "react";
 import Repetition from "./components/Repetition";
 import TokenExample from "./components/TokenExample";
+import Buy from "./components/Buy";
 
 function App() {
-  const alertCompra = () => {
-    window.alert("Compra realizada com sucesso");
-  };
-  const alertVenda = () => {
-    window.alert("Venda realizada com sucesso");
-  };
-
-  const [selectedProduct, setSelectedProduct] = useState("");
-  const [selectedAction, setSelectedAction] = useState("");
-
-  const handleProductChange = (event) => {
-    setSelectedProduct(event.target.value);
-    setSelectedAction(""); // Reset the selected action when the product changes
-  };
-
-  const handleActionChange = (event) => {
-    setSelectedAction(event.target.value);
-  };
-
   return (
     <>
-      <div className="App">
-        <h1>Selecione o produto desejado</h1>
-        <select id="productSelect" onChange={handleProductChange}>
-          <option value="">Selecione um Produto</option>
-          <option value="batata">Batata</option>
-          <option value="mandioca">Mandioca</option>
-          <option value="repolho">Repolho</option>
-        </select>
-        {selectedProduct && (
-          <>
-            <p>O produto selecionado foi {selectedProduct}</p>
-            <label>Selecione a ação desejada</label>
-            <select id="actionSelect" onChange={handleActionChange}>
-              <option value="">Selecione uma Ação</option>
-              <option value="compra">Compra</option>
-              <option value="venda">Venda</option>
-            </select>
-          </>
-        )}
-        {selectedAction && (
-          <>
-            <label>Selecione a opção desejada</label>
-            {selectedAction === "compra" && (
-              <button onClick={alertCompra}>Comprar</button>
-            )}
-            {selectedAction === "venda" && (
-              <button onClick={alertVenda}>Vender</button>
-            )}
-          </>
-        )}
-      </div>
-      <Repetition className='pt-2' />
+      <Buy />
+      <Repetition className="" />
       <TokenExample />
     </>
   );
---
+++ src/components/Buy.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+const Buy = () => {
+  const alertCompra = () => {
+    window.alert("Compra realizada com sucesso");
+  };
+  const alertVenda = () => {
+    window.alert("Venda realizada com sucesso");
+  };
+
+  const [selectedProduct, setSelectedProduct] = useState("");
+  const [selectedAction, setSelectedAction] = useState("");
+
+  const handleProductChange = (event) => {
+    setSelectedProduct(event.target.value);
+    setSelectedAction(""); // Reset the selected action when the product changes
+  };
+
+  const handleActionChange = (event) => {
+    setSelectedAction(event.target.value);
+  };
+
+  return (
+    <div className="p-5 bg-gray-200 w-[500px] rounded-2xl space-y-5">
+      <div>
+        <h1 className="text-2xl pb-2">Selecione o produto desejado</h1>
+        <select id="productSelect" onChange={handleProductChange}>
+          <option value="">Selecione um Produto</option>
+          <option value="batata">Batata</option>
+          <option value="mandioca">Mandioca</option>
+          <option value="repolho">Repolho</option>
+        </select>
+      </div>
+      {selectedProduct && (
+        <>
+          <h2 className="text-xl pb-2">
+            O produto selecionado foi {selectedProduct}
+          </h2>
+          <p>Selecione a ação desejada</p>
+          <select id="actionSelect" onChange={handleActionChange}>
+            <option value="">Selecione uma Ação</option>
+            <option value="compra">Compra</option>
+            <option value="venda">Venda</option>
+          </select>
+        </>
+      )}
+      {selectedAction && (
+        <>
+          <p>A opçao desejada selecionada foi </p>
+          {selectedAction === "compra" && (
+            <button
+              className="px-10 py-2 bg-gray-800 text-white rounded-3xl hover:bg-gray-700"
+              onClick={alertCompra}
+            >
+              Comprar
+            </button>
+          )}
+          {selectedAction === "venda" && (
+            <button
+              className="px-10 py-2 bg-gray-800 text-white rounded-3xl hover:bg-gray-700"
+              onClick={alertVenda}
+            >
+              Vender
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+export default Buy;
