1e34eeaed5888d31f5226601377b4f65f8bdff84
Braulin Pires <braulinpires@hotmail.com>
2023-09-21 23:02:19 +0100

    added new images and set styles for the compoonent Position

--- src/components/Position.js
+++ src/components/Position.js
@@ -2,78 +2,85 @@ import React, { useState } from "react";
 import BlackListener from "../assets/blackListener.png";
 import Escutador from "../assets/escutador.png";
 import Mobile from "../assets/mobile.png";
+import Ferrari from "../assets/ferrari.jpeg";
+import Lamborgini from "../assets/lamborgini.jpeg";
+import RangeRover from "../assets/rangeRover.jpeg";
 
 const arrayTest = [
   {
-    title: "Image 1",
-    description: "description for Image 1",
-    Image: BlackListener,
+    title: "Ferrari",
+    description:
+      "Experience the pinnacle of automotive excellence with our Ferrari collection. Unleash Italian craftsmanship, precision engineering, and breathtaking speed. Elevate your driving passion with a Ferrari – where performance meets artistry.",
+    Image: Ferrari,
   },
   {
-    title: "Image 2",
-    description: "description for Image 2",
-    Image: Escutador,
+    title: "Lamborgini",
+    description:
+      "Unleash your inner daredevil with our Lamborghini selection. Embrace the power, style, and heart-pounding excitement that comes with every Lamborghini drive. Elevate your journey to legendary status with the unmistakable roar of the bull.",
+    Image: Lamborgini,
   },
   {
-    title: "Image 3",
-    description: "description for Image 3",
-    Image: Mobile,
+    title: "Range Rover",
+    description:
+      "Elevate your adventures with our Range Rover lineup. Experience the perfect blend of luxury and off-road prowess, designed for those who demand the extraordinary. From city streets to rugged terrains, Range Rover is your passport to limitless exploration.",
+    Image: RangeRover,
   },
 ];
 
 const Position = () => {
-  const [infoIndex, setInfoIndex] = useState(0); 
+  const [infoIndex, setInfoIndex] = useState(0);
+  const [imageOpacity, setImageOpacity] = useState(0); // Initialize opacity state
 
   const handleChange = (index) => {
     setInfoIndex(index); // Set infoIndex to the clicked index
     console.log("infoIndex:", index);
+    // Fade in the image with a transition effect
+    setImageOpacity(0); // Start with 0 opacity
+    setTimeout(() => {
+      setImageOpacity(1); // Set opacity to 1 after a short delay
+    }, 100); // Adjust the delay (in milliseconds) as needed
+    console.log("Opacity:", imageOpacity);
   };
-  
 
   return (
     <>
-      <div className="flex p-20 text-center">
-        <div className="bg-green-200 flex items-center justify-center rounded-2xl w-[300px] aspect-square">
-          <ul className="space-y-5">
-            {arrayTest.map((item, index) => (
-              <li
-                key={index}
-                onClick={() => handleChange(index)} // Pass the index to handleChange
-                className={`focus:text-red-400 ${
-                    infoIndex === index ? "text-red-400" : ""
+      <div className="w-full h-screen bg-gray-900 relative">
+        <div className="flex text-center   max-h-[900px] py-60 justify-center gap-10">
+          <div className="bg-gray-800 flex items-center justify-around rounded-2xl w-[700px] aspect-square px-16">
+            <ul className="space-y-5">
+              {arrayTest.map((item, index) => (
+                <li
+                  key={index}
+                  onMouseEnter={() => handleChange(index)} // Pass the index to handleChange
+                  className={`focus:text-red-400 ${
+                    infoIndex === index ? "text-orange-700" : "text-gray-500"
                   }`}
-              >
-                {item.title}
-              </li>
-            ))}
-          </ul>
+                >
+                  <h1 className="text-2xl text-left">{item.title}</h1>
+                </li>
+              ))}
+            </ul>
+            <div className="max-w-[200px] text-gray-200 text-left">
+              {arrayTest[infoIndex].description}
+            </div>
+          </div>
+          <div className="relative bg-gray-500 w-[800px] rounded-2xl flex justify-center items-center gap-7">
+            <div className="opacity-50 bg-gradient-to-tr from-gray-300 via-orange-400 to-orange-700 w-[50px] aspect-square rounded-full absolute z-10 right-5 top-5"></div>
+            <div className="w-[600px] aspect-square">
+              <img
+                className="rounded-3xl aspect-square grayscale shadow-md"
+                src={arrayTest[infoIndex].Image}
+              />
+            </div>
+          </div>
         </div>
-        <div className="bg-green-400 w-[600px] rounded-2xl">
-            {arrayTest[infoIndex].description}
+        <div className="flex justify-center -mt-20">
+          <button className="bg-orange-700 hover:bg-orange-600 opacity-60 rounded-full px-40 py-1 text-center text-gray-100">
+            Get your car
+          </button>
         </div>
       </div>
     </>
   );
 };
 export default Position;
-
-
-{
-  /* 
-<div className="p-20 flex">
-        <div className="container relative p-5 bg-white w-[300px] aspect-square rounded-2xl space-y-5 overflow-scroll">
-          <div className="circle bg-blue-500 w-[400px] aspect-square bottom-2 blur-2xl absolute py-10 text-center rounded-full opacity-20 "></div>
-        </div>
-      </div>
- <p>Position</p>
-
-          <p>
-            As plataformas digitais da RTP utilizam cookies próprios e de
-            terceiros (Fornecedores) para melhorar o respetivo desempenho e a
-            experiência do utilizador, melhorar a segurança, evitar fraudes e
-            corrigir erros, armazenando e/ou acedendo a informações no seu
-            dispositivo, e também processando dados pessoais (em detalhe
-            consultar "Política de Cookies" da RTP).
-          </p>
-*/
-}
