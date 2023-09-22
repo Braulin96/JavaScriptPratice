5a45d31f2534e30b6d38bd196bb9ddd6476a965b
Braulin Pires <braulinpires@hotmail.com>
2023-09-22 16:37:27 +0100

    added gap and flex-none to the desciption and title section

--- src/components/Position.js
+++ src/components/Position.js
@@ -12,67 +12,66 @@ const arrayTest = [
     description:
       "Experience the pinnacle of automotive excellence with our Ferrari collection. Unleash Italian craftsmanship, precision engineering, and breathtaking speed. Elevate your driving passion with a Ferrari – where performance meets artistry.",
     Image: Ferrari,
+    background: 'white'
   },
   {
     title: "Lamborgini",
     description:
       "Unleash your inner daredevil with our Lamborghini selection. Embrace the power, style, and heart-pounding excitement that comes with every Lamborghini drive. Elevate your journey to legendary status with the unmistakable roar of the bull.",
     Image: Lamborgini,
+    background: 'black'
   },
   {
     title: "Range Rover",
     description:
       "Elevate your adventures with our Range Rover lineup. Experience the perfect blend of luxury and off-road prowess, designed for those who demand the extraordinary. From city streets to rugged terrains, Range Rover is your passport to limitless exploration.",
     Image: RangeRover,
+    background: 'white'
   },
 ];
 
 const Position = () => {
   const [infoIndex, setInfoIndex] = useState(0);
-  const [imageOpacity, setImageOpacity] = useState(0); // Initialize opacity state
-
+ 
   const handleChange = (index) => {
     setInfoIndex(index); // Set infoIndex to the clicked index
     console.log("infoIndex:", index);
     // Fade in the image with a transition effect
-    setImageOpacity(0); // Start with 0 opacity
-    setTimeout(() => {
-      setImageOpacity(1); // Set opacity to 1 after a short delay
-    }, 100); // Adjust the delay (in milliseconds) as needed
-    console.log("Opacity:", imageOpacity);
   };
 
   return (
     <>
       <div className="w-full h-screen bg-gray-900 relative">
         <div className="flex text-center   max-h-[900px] py-60 justify-center gap-10">
-          <div className="bg-gray-800 flex items-center justify-around rounded-2xl w-[700px] aspect-square px-16">
-            <ul className="space-y-5">
-              {arrayTest.map((item, index) => (
-                <li
-                  key={index}
-                  onMouseEnter={() => handleChange(index)} // Pass the index to handleChange
-                  className={`focus:text-red-400 ${
-                    infoIndex === index ? "text-orange-700" : "text-gray-500"
-                  }`}
-                >
-                  <h1 className="text-2xl text-left">{item.title}</h1>
-                </li>
-              ))}
-            </ul>
-            <div className="max-w-[200px] text-gray-200 text-left">
-              {arrayTest[infoIndex].description}
+            <div className="bg-gray-800 flex items-center justify-around rounded-2xl w-[700px] aspect-square px-16 gap-3">
+                <ul className="space-y-5 flex-none">
+                {arrayTest.map((item, index) => (
+                    <li
+                    key={index}
+                    onMouseEnter={() => handleChange(index)} // Pass the index to handleChange
+                    className={`focus:text-red-400 ${
+                        infoIndex === index ? "text-orange-700" : "text-gray-500"
+                    }`}
+                    >
+                    <h1 className="text-2xl text-left">{item.title}</h1>
+                    </li>
+                ))}
+                </ul>
+                <div className="max-w-[200px] text-gray-200 text-left flex-none">
+                {arrayTest[infoIndex].description}
+                </div>
             </div>
-          </div>
-          <div className="relative bg-gray-500 w-[800px] rounded-2xl flex justify-center items-center gap-7">
-            <div className="opacity-50 bg-gradient-to-tr from-gray-300 via-orange-400 to-orange-700 w-[50px] aspect-square rounded-full absolute z-10 right-5 top-5"></div>
-            <div className="w-[600px] aspect-square">
-              <img
-                className="rounded-3xl aspect-square grayscale shadow-md"
-                src={arrayTest[infoIndex].Image}
-              />
+            <div className="relative bg-gray-500 w-[800px] rounded-2xl flex justify-center items-center gap-7">
+                <div className="opacity-50 bg-gradient-to-tr from-gray-300 via-orange-400 to-orange-700 w-[50px] aspect-square rounded-full 
+                absolute z-10 right-5 top-5 ">
+                </div>
+                <div className="w-[600px] aspect-square">
+                <img
+                    className="rounded-3xl aspect-square grayscale shadow-md"
+                    src={arrayTest[infoIndex].Image}
+                />
+                </div>
             </div>
-          </div>
         </div>
         <div className="flex justify-center -mt-20">
           <button className="bg-orange-700 hover:bg-orange-600 opacity-60 rounded-full px-40 py-1 text-center text-gray-100">
