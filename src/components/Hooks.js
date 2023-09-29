3c4cc9878f2dac822a9ee9f6de9c258a0ff41843
Braulin Pires <braulinpires@hotmail.com>
2023-09-29 10:44:32 +0100

    change photo by using fetch and styling the component

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -1,78 +1,47 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import Denzel from "../assets/denzelWhashington.jpeg";
-import Jackie from "../assets/jackieChan.jpeg";
-import JetLi from "../assets/jetLi.jpeg";
+import React, { useState, useEffect } from "react";
 
 const Hooks = () => {
-  const mobile = [
-    {
-      title: "Iphone",
-      description:
-        "Apple's mobile device that combines an iPod music and video player, mobile phone and Internet browser capability in a handheld unit with a touchscreen interface.",
-    },
-    {
-      title: "Nokia",
-      description:
-        "A Nokia Corporation (pronúncia em finlandês: [ˈnɔkiɑ], nóquia), é uma empresa multinacional finlandesa fundada em 1865",
-    },
-    {
-      title: "Samsung",
-      description:
-        "Samsung Galaxy S9 é um smartphone com sistema operacional Android e da família Galaxy, produzido pela Samsung",
-    },
-  ];
+  const [randomImage, setRandomImage] = useState("");
 
-  const [number, setNumber] = useState("0");
-  const [interactions, setInteractions] = useState(0);
-  const [size, setSize] = useState(0);
-
-  //to get the value of the index
-  const handleChange = (index) => {
-    setNumber(index);
-    console.log("index:", index);
+  const fetchRandomImage = async () => {
+    try {
+      const response = await fetch("https://source.unsplash.com/random");
+      // Get the final URL after any redirects (actual image URL)
+      const imageUrl = response.url;
+      setRandomImage(imageUrl);
+    } catch (error) {
+      console.error("Error fetching random image:", error);
+    }
   };
-
-  //get the value of number of interactions
-  const handleInteractions = () => {
-    setInteractions(interactions + 1);
-  };
-
-  //calling handleInteractions always when the value of number change
   useEffect(() => {
-    handleInteractions();
-    console.log("interactions:", interactions);
-  }, [number]);
+    fetchRandomImage();
+  }, []);
 
   return (
-    <>
-      <div className="bg-green-200 p-10 rounded-xl space-y-5">
-        <h1> UseEffect Task </h1>
-        <div className="flex gap-10">
-          <div className="bg-white rounded-xl p-10">
-            {mobile.map((name, index) => (
-              <p onClick={() => handleChange(index)} key={index}>
-                {" "}
-                {name.title}{" "}
-              </p>
-            ))}
-          </div>
-          <div className="bg-white rounded-xl p-10">
-            {mobile[number].description}
-          </div>
+    <div className="flex justify-center py-10">
+      {/*}
+      {randomImage && <img src={randomImage} alt="Random" />}
+      {!randomImage && <p>Loading random image...</p>}
+  <button onClick={fetchRandomImage}> change </button> */}
+      <div className="p-20 space-y-5 bg-slate-100 w-fit rounded-2xl">
+        <div className="bg-blue-300 border-2 border-blue-00 rounded-full w-36 aspect-square mx-auto flex justify-center items-center overflow-hidden">
+          {randomImage && <img className="w-full h-full object-cover" src={randomImage} alt="Random" />}
+          {!randomImage && <p>Loading random image...</p>}
         </div>
-        <div className="bg-green-400 p-10 rounded-xl space-y-5">
-          <p> Number of interactions: {interactions}</p>
+        <div className="text-center">
           <button
-            className="bg-green-200 rounded-xl px-3 py-2"
-            onClick={() => setInteractions(0)}
+            onClick={fetchRandomImage}
+            className="bg-blue-400 font-bold text-white px-5 py-2 rounded-full"
           >
-            {" "}
-            Clear{" "}
+            Change photo
           </button>
         </div>
+        <div className="flex justify-center">
+          <p className=""> Here goes the array </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
+
 export default Hooks;
