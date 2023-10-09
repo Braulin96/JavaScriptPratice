8f9d20b0067c611cc5fcc26770ea57c31a35e0c6
Braulin Pires <braulinpires@hotmail.com>
2023-10-09 13:45:21 +0100

    create an input number

--- src/components/MoreHooks.js
+++ src/components/MoreHooks.js
@@ -1,17 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ContextTest from "./shared/ContextTest";
 
 // In this component the training is about use Context, use ref and useReducer
-const name = "joao";
-const age = "28";
 
 export const ThemeContext = React.createContext();
+
 const MoreHooks = () => {
+  const [addNumber, setAddNumber] = useState(0);
+
   return (
-    <ThemeContext.Provider value={age}>
-      <div>MoreHooks</div>
-      <ContextTest />
-    </ThemeContext.Provider>
+    <>
+      <ThemeContext.Provider value={addNumber}>
+        <div>MoreHooks</div>
+        <button
+          className="bg-green-500 px-2 py-2"
+          onClick={() => setAddNumber(addNumber + 1)}
+        >
+          increase
+        </button>
+        <ContextTest />
+      </ThemeContext.Provider>
+      <div>
+        <p className="text-xl font-bold">
+          Input testing useState and useContext
+        </p>
+        <div className="p-5">
+          <input
+            className="border border-blue-600 px-3 py-2"
+            type="number"
+            placeholder="hello"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 export default MoreHooks;
