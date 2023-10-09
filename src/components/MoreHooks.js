9cf4c0c8c226d4e86d4f1d901124eef104c91659
Braulin Pires <braulinpires@hotmail.com>
2023-10-09 13:27:03 +0100

    receive a context pass

--- src/components/MoreHooks.js
+++ src/components/MoreHooks.js
@@ -1,10 +1,17 @@
-import { useContext } from "react"
+import React, { useContext } from "react";
+import ContextTest from "./shared/ContextTest";
 
 // In this component the training is about use Context, use ref and useReducer
+const name = "joao";
+const age = "28";
 
+export const ThemeContext = React.createContext();
 const MoreHooks = () => {
   return (
-    <div>MoreHooks</div>
-  )
-}
-export default MoreHooks
+    <ThemeContext.Provider value={age}>
+      <div>MoreHooks</div>
+      <ContextTest />
+    </ThemeContext.Provider>
+  );
+};
+export default MoreHooks;
