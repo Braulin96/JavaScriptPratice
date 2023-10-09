83e09cdde8dad239b9289b22490828011a58f6dc
Braulin Pires <braulinpires@hotmail.com>
2023-10-09 19:56:06 +0100

    add an input focus already in loading page

--- src/components/shared/RefText.js
+++ src/components/shared/RefText.js
@@ -1,40 +1,45 @@
 import React, { useRef, useState, useEffect } from "react";
 
 const RefText = () => {
-  const [name, setName] = useState("");
-  const inputRef = useRef();
-  const count = useRef(0);
-  const [number, setNumber] = useState (0)
+  //const inputRef = useRef();
+  //const count = useRef(0);
+  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
 
-  const focus = () => {
-    inputRef.current.focus();
-  };
+  const numberRef = useRef(0);
+  const buttonRef = useRef();
+  const inputRef = useRef();
 
   useEffect(() => {
-    count.current = count.current + 1;
+    //setNumber ((prevNumber) => prevNumber + 10)
+    numberRef.current = numberRef.current+10;
   });
+  //to start already with a number as clicked and focus in input field
+  useEffect(() => {
+    buttonRef.current.click();
+    inputRef.current.focus();
+  }, []);
 
   return (
-    <div className="p-10">
-      <div>UseRef</div>
-      <input
-        type="text"
-        className="px-5 py-2 border border-gray-400"
-        ref={inputRef}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <p> my name is {name}</p>
+    <div className="bg-gray-200 p-5">
+      <p className="text-xl font-bold"> number is : {number} </p>
+      <p className="text-xl font-bold">
+        number of useRef : {numberRef.current}
+      </p>
+      <p className="text-xl font-bold"> counting is : {count} </p>
       <button
-        className="bg-blue-300 rounded-md border border-blue-400 px-3 py-2"
-        onClick={focus}
+        ref={buttonRef}
+        onClick={() => setCount((prevCount) => prevCount + 1)}
       >
-        Focus
+        Add
       </button>
-      <h1 className="font-bold text-2xl">Render Count: {count.current}</h1>
-      <p>number: {number}</p>
-      <button onClick={() => setNumber((prevNumber) => prevNumber+1)}>Add</button>
-      
+      <div>
+        <input
+          ref={inputRef}
+          type="text"
+          className="bg-blue-100 border border-blue-400 px-5 py-2"
+        />
+      </div>
     </div>
   );
 };
