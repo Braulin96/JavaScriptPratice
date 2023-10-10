643318aff45aa2860cf84dcb7d139cd2c8162de5
Braulin Pires <braulinpires@hotmail.com>
2023-10-10 10:06:21 +0100

    added the ReducerTest component

--- src/components/MoreHooks.js
+++ src/components/MoreHooks.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import ContextTest from "./shared/ContextTest";
 import RefText from "./shared/RefText";
+import ReducerTest from "./shared/ReducerTest";
 
 // In this component the training is about useState useContext, useRef and useReducer
 
@@ -24,43 +25,42 @@ const MoreHooks = () => {
 
   return (
     <>
-    <RefText/>
+      <ReducerTest />
+      <RefText />
       <ThemeContext.Provider value={array}>
         <p className="text-xl font-bold text-center py-2">
           Input testing useState and useContext
         </p>
         <div className="flex justify-center">
-        <div className="flex justify-center bg-gradient-to-br from-slate-600 via-cyan-200 to-gray-700 rounded-xl w-fit px-10 py-5">
-          <div className="p-5 space-y-5">
-            <div>
-              <input
-                onChange={handleNumber}
-                className="border border-gray-500 rounded-md px-3 py-2"
-                type="number"
-                placeholder="insert your code"
-              />
-            </div>
+          <div className="flex justify-center bg-gradient-to-br from-slate-600 via-cyan-200 to-gray-700 rounded-xl w-fit px-10 py-5">
+            <div className="p-5 space-y-5">
+              <div>
+                <input
+                  onChange={handleNumber}
+                  className="border border-gray-500 rounded-md px-3 py-2"
+                  type="number"
+                  placeholder="insert your code"
+                />
+              </div>
 
-            <div>
-              <button
-                onClick={() => {
-                  setFinal(value);
-                  setValue("");
-                }}
-                className="bg-white rounded-full px-3 py-2 text-cyan-900 font-bold"
-              >
-                click me
-              </button>
+              <div>
+                <button
+                  onClick={() => {
+                    setFinal(value);
+                    setValue("");
+                  }}
+                  className="bg-white rounded-full px-3 py-2 text-cyan-900 font-bold"
+                >
+                  click me
+                </button>
+              </div>
+            </div>
+            <div className="py-2">
+              <ContextTest />
             </div>
           </div>
-          <div className="py-2">
-            <ContextTest />
-          </div>
         </div>
-        </div>
-        
       </ThemeContext.Provider>
-      
     </>
   );
 };
