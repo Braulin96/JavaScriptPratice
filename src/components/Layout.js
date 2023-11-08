8d89bdd2e87e1b8bb31a4036ebe2c6b8b814dec3
Braulin Pires <braulinpires@hotmail.com>
2023-11-08 23:00:34 +0000

    set new colors for the text

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,27 +8,33 @@ const Layout = () => {
       <div className="w-full h-screen flex mx-auto justify-center items-center max-w-6xl">
         <div className="w-full h-[700px]">
           <div
-            className="w-full h-full bg-blue-500 bg-cover bg-center flex justify-center items-center "
+            className="w-full h-full bg-blue-500 bg-cover bg-center flex justify-center items-center"
             style={{ backgroundImage: `url(${Nature})` }}
           >
-            <div className="w-[800px] py-8 bg-gray-200 p-4 space-x-4 -translate-y-7">
-              <div className="flex justify-between w-full ">
-                <p className="my-auto ml-4">Life Coaching</p>
-                <div className="flex space-x-4">
-                  <p className="my-auto">Menu</p>
+            <div
+              className="w-[800px] py-4 bg-gray-500 p-4 space-x-4 -translate-y-8"
+              style={{ boxShadow: "0px 0px 4px 0px white" }}
+            >
+              <div className="flex justify-between w-full mb-2">
+                <p className="my-auto ml-4 uppercase font-semibold text-white">
+                  Life Coaching
+                </p>
+                <div className="flex space-x-6 text-white">
+                  <p className="my-auto underline">Menu</p>
                   <p className="my-auto">Login</p>
-                  <button className="border-2 rounded-full border-green-400 px-4 py-2">
-                    Free consultations
-                  </button>
+                  <p className="my-auto">Free Consultations</p>
                 </div>
               </div>
               <div className="flex justify-between w-full">
-              <div className="w-[300px] aspect-square bg-blue-300 bg-center " style={{ backgroundImage: `url(${NatureSmall})` }}></div>
-                <div className="flex flex-col justify-center mx-auto">
+                <div
+                  className="w-[300px] aspect-square bg-blue-300 bg-center "
+                  style={{ backgroundImage: `url(${NatureSmall})` }}
+                ></div>
+                <div className="flex flex-col justify-center mx-auto py-12 text-white space-y-4">
                   <h1 className="text-3xl uppercase font-semibold mb-4">
-                    Merifen Coaching
+                    Front End
                   </h1>
-                  <div className="space-y-2">
+                  <div className="space-y-4">
                     <p className="text-md uppercase text-center">
                       Relationships
                     </p>
@@ -36,10 +42,12 @@ const Layout = () => {
                     <p className="text-md uppercase text-center">Work</p>
                     <p className="text-md uppercase text-center">Life</p>
                   </div>
-                  <button className="border-2 border-blue-500 px-4 py-2 rounded-full mx-auto mt-6"> Work with me </button>
+
+                  <button className="border-2 border-gray-200 px-4 py-1 rounded-full mx-auto">
+                    Work with me
+                  </button>
                 </div>
               </div>
-              
             </div>
           </div>
         </div>
