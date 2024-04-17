8fb028021519a8878c2baf9706e651cfe7ca4dc7
Braulin Pires <braulinpires@hotmail.com>
2024-04-17 16:55:27 +0100

    added validation for the checkbox terms and privacy

--- src/components/jbCole/HookForm.js
+++ src/components/jbCole/HookForm.js
@@ -2,11 +2,15 @@ import { useForm } from "react-hook-form";
 
 const HookForm = () => {
   //we use the register function to register the inputs values, handleSubmit to validate the values, formState-errors
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const onSubmit = (data) => {
     console.log("data:", data);
   };
-  console.log (errors)
+  console.log(errors);
 
   return (
     <div className="flex flex-col mt-4 gap-y-6">
@@ -14,55 +18,99 @@ const HookForm = () => {
         <label>Name</label>
         <input
           //className="border-blue-400 outline-none border p-2 rounded-lg w-64"
-          className={`outline-none border p-2 rounded-lg w-64 ${ errors?.name ? 'border-red-400' : 'border-blue-400 '}`}
+          className={`outline-none border p-2 rounded-lg w-64 ${
+            errors?.name ? "border-red-400" : "border-blue-400 "
+          }`}
           type="text"
           placeholder="Insert your name..."
           {...register("name", { required: true })}
         />
-        { errors?.name?.type === 'required' ? <p className="text-sm text-red-400">Name is required</p> : ''}
+        {errors?.name?.type === "required" ? (
+          <p className="text-sm text-red-400">Name is required</p>
+        ) : (
+          ""
+        )}
       </div>
       <div className="form-group flex flex-col gap-y-2 mx-auto">
         <label>Email</label>
         <input
-          className="border-blue-400 outline-none border p-2 rounded-lg w-64"
+          //className="border-blue-400 outline-none border p-2 rounded-lg w-64"
+          className={`outline-none border p-2 rounded-lg w-64 ${
+            errors?.email ? "border-red-400" : "border-blue-400 "
+          } `}
           type="text"
           placeholder="Insert your email..."
           {...register("email", { required: true })}
         />
+        {errors?.email?.type === "required" ? (
+          <p className="text-sm text-red-400">Email is required</p>
+        ) : (
+          ""
+        )}
       </div>
 
       <div className="form-group flex flex-col gap-y-2 mx-auto">
         <label>Password</label>
         <input
-          className={`outline-none border p-2 rounded-lg w-64 ${ errors?.password ? 'border-red-400' : 'border-blue-400 '}`}
+          className={`outline-none border p-2 rounded-lg w-64 ${
+            errors?.password ? "border-red-400" : "border-blue-400 "
+          }`}
           type="text"
           placeholder="type password..."
-          {...register("password", { required:true, minLength: 7 })}
+          {...register("password", { required: true, minLength: 7 })}
         />
-           { errors?.password?.type === 'minLength' ? <p className="text-sm text-red-400">At least 7 characters</p> : ''}
-           { errors?.password?.type === 'required' ? <p className="text-sm text-red-400">Password is required</p> : ''}
+        {errors?.password?.type === "minLength" ? (
+          <p className="text-sm text-red-400">At least 7 characters</p>
+        ) : (
+          ""
+        )}
+        {errors?.password?.type === "required" ? (
+          <p className="text-sm text-red-400">Password is required</p>
+        ) : (
+          ""
+        )}
       </div>
 
       <div className="form-group flex flex-col gap-y-2 mx-auto">
         <label>Profession</label>
         <select
-          {...register("profession")}
-          className="border-blue-400 outline-none border p-2 rounded-lg w-64"
+          {...register("profession", {
+            validate: (value) => {
+              return value !== "0";
+            },
+          })}
+          className={`outline-none border p-2 rounded-lg w-64 ${
+            errors?.profession ? "border-red-400" : "border-blue-400 "
+          }`}
         >
           <option value="0">Select your profession</option>
           <option value="developer">Developer</option>
           <option value="designer">Designer</option>
         </select>
+        {errors?.profession?.type === "validate" ? (
+          <p className="text-sm text-red-400">Profession is required</p>
+        ) : (
+          ""
+        )}
       </div>
 
-      <div className="checkbox-group flex  gap-x-2 mx-auto">
+      <div className="checkbox-group flex flex-col gap-x-2 mx-auto">
+        <div className="flex gap-x-2">
         <input
           //className="border-blue-400 outline-none border p-2 rounded-lg w-64"
           type="checkbox"
           name="privacy-policy"
-          {...register("privacyTerms")}
+          {...register("privacyTerms", { required: true })}
         />
         <label> I agree with the privacy terms</label>
+
+        </div>
+        
+        {errors?.privacyTerms?.type === "required" ? (
+          <p className="text-sm text-red-400">Agree with terms is required</p>
+        ) : (
+          ""
+        )}
       </div>
 
       {/* <button
